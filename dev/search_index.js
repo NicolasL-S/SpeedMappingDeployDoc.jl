c0f6c56cd65840ef60beaf61f4d2155da1b5f001
Documenter.jl <documenter@juliadocs.github.io>
2025-08-18 15:49:25 +0000

    build based on d1a01ed

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SpeedMappingDeployDoc","page":"Home","title":"SpeedMappingDeployDoc","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SpeedMappingDeployDoc.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
