c1c19cb00803de174494ccb20b3b5b491409386f
Nicolas Lepage-Saucier <nicolas.lepagesaucier@gmail.com>
2025-08-20 16:14:32 -0400

    More desperation

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,8 @@ using SpeedMappingDeployDoc
 using Documenter
 using Literate
 using Unitful
+using Pkg
+Pkg.add(url = "https://github.com/NicolasL-S/SpeedMapping.jl")
 using SpeedMapping
 
 DocMeta.setdocmeta!(SpeedMappingDeployDoc, :DocTestSetup, :(using SpeedMappingDeployDoc); recursive=true)
