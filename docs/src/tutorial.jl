89b9cba187da8acc4ca0936d78d32b92b0a0c453
Nicolas Lepage-Saucier <nicolas.lepagesaucier@gmail.com>
2025-08-21 00:29:26 -0400

    Update tutorial.jl

--- docs/src/tutorial.jl
+++ docs/src/tutorial.jl
@@ -111,10 +111,11 @@ res_with_objective = speedmapping([0.25, 1., 2.]; f = neg_log_likelihood, m! = E
     lower = [0.,0.,0.], upper = [1., Inf, Inf], buffer = 0.05);
 
 # Here, the keyword argument `buffer` (= 0.05 by default for mapping applications) ensures that a 
-# parameter xᵢ will be at least at a distance 0.05 × |xᵢlast - boundᵢ| of boundᵢ, where xᵢlast is xᵢ's 
-# last value. This safeguard avoids jumping to a bound instantly (unless buffer = 0). For instance, 
-# if xᵢlast $= 0.2$, AA's next iterate is xᵢtry $= -0.1$, and lowerᵢ $= 0$, then xᵢ is set to 
-# max$($xᵢtry, buffer $×$ xᵢlast $+ (1 -$ buffer$) ×$ lowerᵢ$) =$ max$(-0.1, 0.05 × 0.2 + 0.95 × 0.) = 0.01$.
+# parameter xᵢ will be at least at a distance 0.05 × |xᵢlast - boundᵢ| of boundᵢ, where xᵢlast is 
+# xᵢ's last value. This safeguard avoids jumping to a bound instantly (unless buffer = 0), in case 
+# bound is infeasible or is a saddle point. For instance, if xᵢlast $= 0.2$, lowerᵢ $= 0$, but AA's 
+# next iterate is xᵢtry $= -0.1$, then xᵢ is set to 
+# max$($xᵢtry, buffer $×$ xᵢlast $+ (1 -$ buffer$) ×$ lowerᵢ$) =$ max$(-0.1, 0.05 × 0.2 + 0.95 × 0) = 0.01$.
 #
 # ## Avoiding memory allocation
 #
